584746e499e0a852bc978b495d4ffedae898face
Linawaty <linawatylim@gmail.com>
2020-05-22 22:41:10 +0800

    Use form get method instead of URL redirection

--- public/index.js
+++
@@ -1,5 +0,0 @@
-const setFilterURL = () => {
-    var url= `/filter?state=${document.getElementById('state').value}&city=${document.getElementById('city').value}&date=${document.getElementById('date').value}&shape=${document.getElementById('shape').value}`;
-    document.write("Redirecting to the url now..."); 
-    setTimeout(function(){window.location = url;}, 10); 
-}
